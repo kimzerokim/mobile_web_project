e6dd5fd682f78f51430d45c01d274a2a4e6cf30e
YoungKim <ky200223@gmail.com>
2014-09-18 12:57:29 +0900

    change left to transform

--- horizontal.js
+++ horizontal.js
@@ -53,36 +53,161 @@
     initPosition();
 })();
 
+//var Flicking = {
+//    init: function () {
+//        this.width = window.innerWidth;
+//        this.container = document.querySelector("#feedFlow ul");
+//        this.threadWidth = parseFloat(document.getElementById('card1').style.width) +
+//            parseFloat(document.getElementById('card1').style.marginRight);
+//        this.halfWidth = this.width / 2;
+//        this.finalLeft = parseFloat(this.container.style.left);
+//        this.touchStartX = 0;
+//        this.touchStartY = 0;
+//        this.dragX = 0;
+//        this.dragY = 0;
+//        this.sideDrag = false;
+//        this.upDrag = false;
+//        this.eventBind();
+//    },
+//    eventBind: function () {
+//        //touch, requestAnimationFrame
+//        this.container.addEventListener("touchstart", this.touchStart.bind(this));
+//        this.container.addEventListener("touchmove", this.touchMove.bind(this));
+//        this.container.addEventListener("touchend", this.touchEnd.bind(this));
+//    },
+//    touchStart: function (e) {
+//        this.touchStartX = e.touches[0].pageX;
+//        this.touchStartY = e.touches[0].pageY;
+//        this.container.style.left = this.finalLeft + 'px';
+////        this.container.style.transition = '';
+////        this.container.style.transform = 'translate3d(0, 0, 0)';
+//        this.container.style.webkitTransition = '';
+//        this.container.style.webkitTransform = 'translate3d(0, 0, 0)';
+//    },
+//    touchMove: function (e) {
+//        e.preventDefault();
+//        this.dragX = e.changedTouches[0].pageX - this.touchStartX;
+//        this.dragY = e.changedTouches[0].pageY - this.touchStartY;
+//        this.detectDirection(this.dragX, this.dragY);
+//    },
+//    detectDirection: function (x, y) {
+//        var upSlope = Math.abs(window.innerHeight / (window.innerWidth / 2)),
+//            sideSlope = Math.abs((window.innerHeight / 2) / window.innerWidth),
+//            userSlope = Math.abs(y / x),
+//            distance = Math.sqrt(x * x + y * y);
+//
+//        if (distance > 10 && userSlope >= upSlope) {
+//            this.upDrag = true;
+//        } else if (sideSlope >= userSlope) {
+//            this.sideDrag = true;
+//            this.container.style.left = this.finalLeft + this.dragX + 'px';
+//        } else {
+//            this.upDrag = false;
+//            this.sideDrag = false;
+//        }
+//    },
+//    touchEnd: function (e) {
+//        var touchElement = e.changedTouches[0].target.parentNode;
+//        if (touchElement === document.getElementById('feedFlow')) {
+//            touchElement = null;
+//        }
+//        if (this.upDrag === true && touchElement != null) {
+//            this.deleteCard(touchElement);
+//        } else if (this.sideDrag === true) {
+//            if (Math.abs(this.dragX) > this.halfWidth * 0.5) {
+//                if (this.dragX < 0) {
+//                    //left
+//                    this.animate(-(this.threadWidth + this.dragX));
+//                    this.swapCard('left');
+//                } else {
+//                    //right
+//                    this.animate(this.threadWidth - this.dragX);
+//                    this.swapCard('right');
+//                }
+//            } else {
+//                //console.log('go back')
+//                this.animate(-this.dragX);
+//            }
+//        }
+//        this.touchStartX = 0;
+//        this.touchStartY = 0;
+//        this.dragX = 0;
+//        this.dragY = 0;
+//        this.upDrag = false;
+//        this.sideDrag = false;
+//    },
+//    animate: function (distance) {
+////        this.container.style.transition = 'transform 0.3s ease-in-out';
+////        this.container.style.transform = 'translate3d(' + distance + 'px, 0, 0)';
+//        this.container.style.webkitTransition = '-webkit-transform 0.3s ease-in-out';
+//        this.container.style.webkitTransform = 'translate3d(' + distance + 'px, 0, 0)';
+//    },
+//    swapCard: function (direction) {
+//        var threadFlowLeft = parseFloat(this.container.style.left),
+//            threadList = document.getElementsByClassName('threadContainer'),
+//            firstElement = threadList[0],
+//            lastElement = threadList[4];
+//
+//        if (direction === 'left') {
+//            var duplicateNode = firstElement.cloneNode(true);
+//            this.container.appendChild(duplicateNode);
+//            this.container.style.left = (threadFlowLeft + this.threadWidth) + 'px';
+//            this.container.removeChild(firstElement);
+//        } else if (direction === 'right') {
+//            var duplicateNode = lastElement.cloneNode(true);
+//            this.container.insertBefore(duplicateNode, firstElement);
+//            this.container.style.left = (threadFlowLeft - this.threadWidth) + 'px';
+//            this.container.removeChild(lastElement);
+//        }
+//    },
+//    deleteCard: function (touchElement) {
+//        var duplicateNode = touchElement.cloneNode(true);
+//        //var deleteNextElement = touchElement.nextSibling.nextSibling;
+//        this.container.removeChild(touchElement);
+//        this.container.appendChild(duplicateNode);
+//    }
+//};
+//
+//Flicking.init();
+
 var Flicking = {
     init: function () {
         this.width = window.innerWidth;
-        this.container = document.querySelector("#feedFlow ul");
+        this.halfWidth = this.width / 2;
         this.threadWidth = parseFloat(document.getElementById('card1').style.width) +
             parseFloat(document.getElementById('card1').style.marginRight);
-        this.halfWidth = this.width / 2;
-        this.finalLeft = parseFloat(this.container.style.left);
+        this.container = document.querySelector("#feedFlow ul");
+        this.container.style.webkitTransition = '-webkit-transform 0.3s ease-in-out';
+        this.container.style.webkitTransform = 'translate3d(0, 0, 0)';
+        this.cardList = ['card1', 'card2', 'card3', 'card4', 'card5'];
+        this.finalLeft = 0;
         this.touchStartX = 0;
         this.touchStartY = 0;
         this.dragX = 0;
         this.dragY = 0;
         this.sideDrag = false;
         this.upDrag = false;
-        this.eventBind();
+        this.touchEventBind();
     },
-    eventBind: function () {
-        //touch, requestAnimationFrame
+    cardFlip: function (e) {
+        var card = e.target.parentNode.parentNode;
+        card.style.webkitTransform = 'rotateY(180deg)';
+        console.log(e.target.parentNode.parentNode);
+    },
+    touchEventBind: function () {
         this.container.addEventListener("touchstart", this.touchStart.bind(this));
         this.container.addEventListener("touchmove", this.touchMove.bind(this));
         this.container.addEventListener("touchend", this.touchEnd.bind(this));
+        this.cardList.forEach(function (cardName) {
+            var card = document.getElementById(cardName),
+                moreButton = card.childNodes[3].childNodes[1];
+            moreButton.addEventListener('click', Flicking.cardFlip.bind(Flicking));
+        });
     },
     touchStart: function (e) {
         this.touchStartX = e.touches[0].pageX;
         this.touchStartY = e.touches[0].pageY;
-        this.container.style.left = this.finalLeft + 'px';
-//        this.container.style.transition = '';
-//        this.container.style.transform = 'translate3d(0, 0, 0)';
-        this.container.style.webkitTransition = '';
-        this.container.style.webkitTransform = 'translate3d(0, 0, 0)';
+        this.animate(this.finalLeft);
     },
     touchMove: function (e) {
         e.preventDefault();
@@ -96,11 +221,11 @@ var Flicking = {
             userSlope = Math.abs(y / x),
             distance = Math.sqrt(x * x + y * y);
 
-        if (distance > 10 && userSlope >= upSlope) {
+        if (distance > 4 && userSlope >= upSlope) {
             this.upDrag = true;
         } else if (sideSlope >= userSlope) {
             this.sideDrag = true;
-            this.container.style.left = this.finalLeft + this.dragX + 'px';
+            this.animate(this.finalLeft + this.dragX);
         } else {
             this.upDrag = false;
             this.sideDrag = false;
@@ -117,16 +242,17 @@ var Flicking = {
             if (Math.abs(this.dragX) > this.halfWidth * 0.5) {
                 if (this.dragX < 0) {
                     //left
-                    this.animate(-(this.threadWidth + this.dragX));
+                    this.animate(this.finalLeft - this.threadWidth);
+                    this.finalLeft -= this.threadWidth;
                     this.swapCard('left');
                 } else {
                     //right
-                    this.animate(this.threadWidth - this.dragX);
+                    this.animate(this.finalLeft + this.threadWidth);
+                    this.finalLeft += this.threadWidth;
                     this.swapCard('right');
                 }
             } else {
-                //console.log('go back')
-                this.animate(-this.dragX);
+                this.animate(this.finalLeft);
             }
         }
         this.touchStartX = 0;
@@ -137,9 +263,6 @@ var Flicking = {
         this.sideDrag = false;
     },
     animate: function (distance) {
-//        this.container.style.transition = 'transform 0.3s ease-in-out';
-//        this.container.style.transform = 'translate3d(' + distance + 'px, 0, 0)';
-        this.container.style.webkitTransition = '-webkit-transform 0.3s ease-in-out';
         this.container.style.webkitTransform = 'translate3d(' + distance + 'px, 0, 0)';
     },
     swapCard: function (direction) {
@@ -162,7 +285,6 @@ var Flicking = {
     },
     deleteCard: function (touchElement) {
         var duplicateNode = touchElement.cloneNode(true);
-        //var deleteNextElement = touchElement.nextSibling.nextSibling;
         this.container.removeChild(touchElement);
         this.container.appendChild(duplicateNode);
     }
